1995c62763283a25981e0c0131d02d17ac247a8b
amit Kumar <amit.k001122@gmail.com>
2025-11-14 15:39:45 +0530

    Hero section Updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -11,10 +11,7 @@ const Home = () => {
   return (
     <div
       id='home'
-      className='relative mt-14 md:mt-20 h-40 sm:h-[250px] md:h-[300px] lg:h-[350px] xl:h-[500px] bg-cover bg-center flex items-center shadow-xl'
-      // h-[150px] sm:h-[250px] md:h-[60vh] xl:h-[80vh] 
-      //             bg-cover bg-center flex items-center justify-center 
-      //            w-full overflow-hidden shadow-xl mt-15 md:mt-0'
+      className='relative mt-14 md:mt-20 h-[85vh]   bg-cover bg-center lg:flex items-center shadow-xl'
       style={{ backgroundImage: `url(${homeimg})` }}
     >
       {/* Lead Form Modal */}
@@ -29,41 +26,35 @@ const Home = () => {
         transition={{ duration: 1.5 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
-        className='container text-center sm:mx-auto py-4 px-6 lg:px-16 xl:px-32 text-white z-10'
+        className='container text-center sm:mx-auto py-16 sm:px-10 md:px-26   lg:px-16 xl:px-32 text-white z-10'
       >
-        <h2 className='text-lg max-w-60 sm:max-w-100 md:max-w-140 lg:max-w-160 xl:max-w-200 
+        <h2 className='text-3xl   md:max-w-140 lg:max-w-160 xl:max-w-200 
                        sm:text-3xl md:text-4xl lg:text-6xl font-extrabold pt-2 leading-tight'>
           Explore smart spaces that fit your lifestyle
         </h2>
 
-        <div className='mt-5 md:mt-16 flex text-[10px] sm:text-lg sm:flex-row gap-2 sm:gap-4 
-                        pl-16 md:pl-28 lg:pl-36 xl:pl-72 items-center xl:max-w-5xl'>
+        <div className='mt-10 md:mt-16 flex justify-center text-base sm:text-lg lg:text-2xl sm:flex-row gap-2 sm:gap-4 
+                         md:pl-16  lg:pl-0 items-center lg:max-w-2xl xl:max-w-3xl'>
           <button
             onClick={() => setIsFormOpen(true)}
-            className='border bg-gray-900 opacity-90 border-white py-1 px-2 sm:px-4 sm:py-2 
-                       md:py-3 md:w-auto rounded cursor-pointer'
+            className='border bg-gray-900 opacity-90 border-white py-2 px-3 sm:px-4 sm:py-2 
+                       md:py-3 md:w-auto  rounded cursor-pointer'
           >
             Request Consultation
           </button>
-          {/* <a
-            href="#contact"
-            className='border border-white bg-gray-900 opacity-90 py-1 px-2 sm:px-4 sm:py-2 
-                       md:px-8 md:py-3 rounded'
-          >
-            Contact Us
-          </a> */}
+          
         </div>
       </motion.div>
 
       {/* Promotional banner on right side */}
       <div
-        className='absolute right-0 bottom-5 md:bottom-0   
-                   flex justify-end items-end z-20 w-[40%] sm:w-[35%] md:w-[30%] lg:w-[35%]'
+        className='absolute bottom-[8%] right-[28%] sm:right-[35%] sm:bottom-0  md:right-[30%]  lg:right-0 lg:bottom-[30%] xl:bottom-10  xl:right-0
+                   flex  sm:justify-end items-center text-center z-20 w-[180px] sm:w-[35%] md:w-[30%] lg:w-[35%]'
       >
         <img
           src={promoBanner}
           alt="Promotion Banner"
-          className='w-24 sm:w-52 md:w-60 lg:w-72 xl:w-full h-auto  object-contain rounded-full xl:rounded drop-shadow-xl'
+          className='w-52 sm:w-52 md:w-72 lg:w-72 xl:w-full h-auto  object-contain rounded-full drop-shadow-xl'
         />
       </div>
     </div>
